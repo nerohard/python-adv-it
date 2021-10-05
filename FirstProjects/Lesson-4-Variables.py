6f2d952ca1044f6a5fff776027798ede4f2ec637
nerohard <ner0ohard@gmail.com>
2021-10-05 21:11:30 +0300

    Initial commit

---
+++ FirstProjects/Lesson-4-Variables.py
@@ -0,0 +1,23 @@
+a = "Hello"
+b = 25
+
+print(a)
+print(b)
+
+f_name = "Dmytro"
+l_name = "Petryshyn"
+age = 23
+full_name = f_name + " " + l_name
+
+print(full_name + ", " + str(age))
+
+f_name = "denis"
+F_NAME = "DENIS"
+
+print(f_name, F_NAME)
+
+num1 = 11111111111111
+num2 = 44444444444444
+num3 = num1 + num2
+
+print(num3)
